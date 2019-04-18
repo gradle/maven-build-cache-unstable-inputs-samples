a20019e0cfb00ceec30c395b9d3a045b693aeb5c
Marc Philipp <marc@gradle.com>
2019-04-18 11:49:27 +0200

    Pass samplesDir as undeclared input

--- impl/src/test/java/com/gradle/example/impl/ImplTest.java
+++ impl/src/test/java/com/gradle/example/impl/ImplTest.java
@@ -1,12 +1,31 @@
 package com.gradle.example.impl;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+
 import org.junit.Test;
-import static org.junit.Assert.assertEquals
-;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class ImplTest {
-    
+
+    @Parameters
+    public static File[] files() {
+        return new File(System.getProperty("samplesDir")).listFiles();
+    }
+
+    @Parameter(0)
+    public File sampleFile;
+
     @Test
-    public void testTheAnswer() {
-        assertEquals(42, new Impl().getTheAnswer());
+    public void testTheAnswer() throws Exception {
+        List<String> lines = Files.readAllLines(sampleFile.toPath());
+        assertEquals("42", lines.get(0));
     }
 }
